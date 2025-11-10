ef814a9e0c1069671552b92f870768f6c9707420
Fernando Figaro <70062249+Fernando-olv@users.noreply.github.com>
2025-11-10 08:26:47 -0300

    Refactor buscar_cep.py with API suggestion comment

--- buscar_cep.py
+++ buscar_cep.py
@@ -1,8 +1,11 @@
 import requests
 
+#Sugestão de API: https://cep.awesomeapi.com.br/json/
+
 def buscar_cep(cep_usuario):
     # Recebe um CEP e devolve o endereço de uma API
     print()
 def descobrir_cep(endereco_usuario):
    # Recebe um endereço e pesquisa no Google esse endereço em busca do CEP.
+
    print()
